d0412820b2dbc067e5d124ad25e74ef8ec7b519e
mahesh <maheshkuraganti9@gmail.com>
2024-05-29 12:51:51 +0530

    versions commited

---
+++ practicepurpose/versions.tf
@@ -0,0 +1,15 @@
+#Terraform Block
+terraform {
+  required_version = "~>1.8.4"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.51.1"
+    }
+  }
+}
+#Provider Block
+provider "aws" {
+  region = "ap-south-1"
+  profile = "default"
+}
